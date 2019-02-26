ef411d3fd5d3ad3a274271b81a570772890a31fc
Clayton Wang <claydwang@gmail.com>
2019-02-25 21:23:38 -0800

    changed root folder name to app

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ io.on('connection', socket => {
 });
 
 app.use('*', (req, res) => {
-  res.sendFile(`${__dirname}/connect-four/connect/build/index.html`);
+  res.sendFile(`${__dirname}/connect/build/index.html`);
 });
 
 server.listen(PORT, err => {
