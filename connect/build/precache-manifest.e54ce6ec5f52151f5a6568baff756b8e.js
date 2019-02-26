3bfd131d7c732cb44b59f156bf3954f8322ba2b8
Clayton Wang <claydwang@gmail.com>
2019-02-25 21:33:06 -0800

    removed build

---
+++ connect/build/precache-manifest.e54ce6ec5f52151f5a6568baff756b8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "eb02a8c9b818f3a02efb",
+    "url": "/static/js/main.eb02a8c9.chunk.js"
+  },
+  {
+    "revision": "de12df015efe7d6798e5",
+    "url": "/static/js/2.de12df01.chunk.js"
+  },
+  {
+    "revision": "eb02a8c9b818f3a02efb",
+    "url": "/static/css/main.140b704e.chunk.css"
+  },
+  {
+    "revision": "63290b50e75159f84e0dd4ac56a08f13",
+    "url": "/index.html"
+  }
+];
